f5d2d995abbf23414a470587a560e2d1bfefc60a
Maryna-Zakrevska <lovemama.com.ua@gmail.com>
2021-10-26 18:54:49 +0300

    ДЗ8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css'
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function createGallery (items) {
+
+    return galleryItems.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    }).join('');
+ 
+ }
+
+ const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250});
+
+
+
+
