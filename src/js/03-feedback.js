26a1581784e83b6d0d532df6f1a09142bf88f62a
Maryna-Zakrevska <lovemama.com.ua@gmail.com>
2021-10-27 02:43:56 +0300

    ДЗ8+-

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+const formRef = document.querySelector('.feedback-form');
 
+const savedData = () => JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (savedData()) {formRef.email.value  = savedData().email;
+  formRef.message.value  = savedData().message;} else {const formDataInitial = {
+    email: '',
+    message: '',
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formDataInitial));}
+
+
+formRef.addEventListener('input', callback);
+function callback(event) {
+    const { email, message } = event.currentTarget;
+    
+    const formData = {
+      email: email.value,
+      message: message.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));}
+
+
+    formRef.addEventListener('submit', onFormSubmit);
+    function onFormSubmit(event) {event.preventDefault(); console.log(savedData()); localStorage.removeItem('feedback-form-state'); }
